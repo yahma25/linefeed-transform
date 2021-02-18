5aeea54ca3a9d2a13f145b51ae2e22b076cce032
mhkim <yahma25@gmail.com>
2021-02-18 23:03:50 +0900

    스피커 멘트 제거하는 기능 추가
    ex) MyoungHo Kim: Hello -> Hello

---
+++ remove_speaker.py
@@ -0,0 +1,35 @@
+import os
+
+PATH_DIR = './'
+
+fileList = os.listdir(PATH_DIR)
+
+
+def parse_lines(lines):
+    # 각 라인들의 ':' 문자를 찾아서 이전 문자들과 다음 여백까지 제거
+    # ex) Heena Bawazir: Hello everybody, good evening.
+    # -> "Heena Bawazir: " 제거
+    # -> Hello everybody, good evening.
+    new_lines = []
+    for line in lines:
+        foundIdx = line.find(':')
+        if (foundIdx > -1):
+            new_line = line[foundIdx + 2:]
+            new_lines.append(new_line)
+        else:
+            new_lines.append(line)
+    return new_lines
+
+
+for fileName in fileList:
+    # splitext()의 [0] = 파일 이름, [1] = 확장자
+    if os.path.splitext(fileName)[1] != '.txt':
+        continue
+
+    rf = open(PATH_DIR + fileName, 'r', encoding='utf-8')
+    newLines = parse_lines(rf.readlines())
+    rf.close()
+
+    wf = open(PATH_DIR + '변환_' + fileName, 'w', encoding='utf-8')
+    wf.writelines(newLines)
+    wf.close()
